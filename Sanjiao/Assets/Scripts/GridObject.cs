13ecbf278b0ba2597e4bc397b061fde45a241209
KyleKK04 <Kyle20040930@Gmail.com>
2025-11-25 13:03:58 +0800

    [程序]新建GridObject，DataStructure

---
+++ Sanjiao/Assets/Scripts/GridObject.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 这是所有地图物体的父类。包含DataStructure里的坐标位置，方向，是否阻挡移动，是否可移动的判断
+/// </summary>
+public class GridObject : MonoBehaviour
+{
+    public GridCoordinates gridCoordinates;
+    public Direction direction;
+    public bool isBlockingMovement = false;
+    public bool isMovable = false;
+
+    public GridObject(int x, int y, Direction dir)
+    {
+        gridCoordinates = new GridCoordinates(x, y);
+        direction = dir;
+    }
+    
+    public void Interact()
+    {
+        //按E键交互
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //TODO:待完成交互逻辑   
+        }
+    }
+
+    public void OnChant(int powerLevel,Direction inputDir)
+    {
+        //TODO:被击中
+    }
+}
